f2987ccd125c2efb8e5d6c9549356d4a1d0348a3
C. Arpe <colin.j.arpe@outlook.com>
2018-12-24 23:58:24 -0500

    Build listening functionality

--- Assets/Scripts/scripts.js
+++ Assets/Scripts/scripts.js
@@ -1,18 +1,31 @@
 const MIN = 1;
 const MAX = 9;
-
+const LIGHTER = 0.25;
 $(document).ready(() => {
     let boxArray = [];
     fillBoard(boxArray);
     sortArray(boxArray);
-    listenToBoard($('.gameboard'));
+    listenToBoard($('.gameboard'), boxArray);
 });
 
-function listenToBoard($board)  {
-    return;
-    // $('.gameboard__cell', $board).on('click'(i, cell) => {
+function listenToBoard($board, arr)  {
+    let count = 0;
+    $('.gameboard__cell', $board).on('click', (e) => {
+        let thisCell = e.currentTarget;
+        if (thisCell == arr[count]["cell"][0]) {
+            $(thisCell).css("opacity", 1);
+            count++;
+        }   else    {
+            $(thisCell).css("background-color", "#000");
+            $('.gameboard__text', thisCell).css("color", "black");
+        }
+    })
+}
 
-    // })
+function sortArray(arr) {
+    arr.sort((a, b) => {
+        return a["value"] - b["value"];
+    })
 }
 
 function fillBoard(arr) {
@@ -22,7 +35,7 @@ function fillBoard(arr) {
     }
 
     $('.gameboard__text').each((i, box) => {
-        numArray.sort(function(a, b){return 0.5 - Math.random()});
+        numArray.sort((a, b) => {return 0.5 - Math.random()});
         let thisNum;
         if (0.5 - Math.random() > 0) {
             thisNum = numArray.shift();
@@ -32,18 +45,18 @@ function fillBoard(arr) {
 
         $(box).text(thisNum);
 
-        arr[i] = [];
-        arr[i][0] = thisNum;
-        arr[i][1] = box;
-
         let $cell = $(box).closest('.gameboard__cell');
         $cell.css("background-color", makeColor(thisNum));
+
+        arr[i] = [];
+        arr[i]["value"] = thisNum;
+        arr[i]["cell"] = $cell;
     });
 }
 
 function makeColor(x)   {
     let colorArray = [];
-    colorArray[x % 3] = ((x / (MAX * 2)) / 2) * 255;
+    colorArray[x % 3] = (((x / (MAX * 2)) / 2) + LIGHTER) * 255;
     colorArray[(x + 1) % 3] = (x / (MAX * 2)) * 255;
     colorArray[(x + 2) % 3] = ((x / (MAX * 2)) * 2)   * 255;
 
